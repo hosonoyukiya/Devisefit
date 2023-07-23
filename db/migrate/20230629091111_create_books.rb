7fd6bf37b091e2b1e71cd33ea423e8abb3fbae06
EC2 Default User <ec2-user@ip-172-31-3-152.ap-northeast-1.compute.internal>
2023-07-23 09:45:40 +0000

    [Fix] Migration

--- db/migrate/20230629091111_create_books.rb
+++ db/migrate/20230629091111_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title, null: false #タイトル
       t.text :body, null: false #感想
-      t.references :user, null: false, foreign_key: true #投稿したユーザー　外部キー
+      t.integer :user #投稿したユーザー　外部キー
       t.timestamps
     end
   end
